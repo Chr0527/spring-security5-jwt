846a66d371de2b1d84d022f28a970683f9cf8937
Chr0527 <1015670600@qq.com>
2019-07-02 15:57:13 +0800

    init

--- src/main/java/com/example/springsecurity/springsecurity/MyJwtTokenFilter.java
+++ src/main/java/com/example/springsecurity/springsecurity/MyJwtTokenFilter.java
@@ -18,8 +18,13 @@ import java.io.IOException;
 /**
  * token 过滤器，在这里解析token，拿到该用户角色，设置到springsecurity的上下文环境中，让springsecurity自动判断权限
  * 所有请求最先进入此过滤器，包括登录接口，而且在springsecurity的密码验证之前执行
- *  * GYB
- *  * 20190220
+ * <p>
+ * MyJwtTokenFilter 类继承自OncePerRequestFilter，这个OncePerRequestFilter就厉害了，可以说是所有filter的基类，所以最先执行，
+ * 在这里我们写验证jwt的逻辑，验证通过后要告诉springsecurity，我们获取到的用户的权限，并把权限设置到springsecurity的上下文环境中，
+ * 让它来给我们做权限的判断，这点最重要！！！！是springsecurity和jwt的整合纽带
+ * <p>
+ * * GYB
+ * * 20190220
  */
 @Component
 public class MyJwtTokenFilter extends OncePerRequestFilter {
@@ -42,9 +47,9 @@ public class MyJwtTokenFilter extends OncePerRequestFilter {
             System.out.println("username:" + username);
             //验证token,具体怎么验证看需求，可以只验证token不查库，把权限放在jwt中即可
             UserDetails UserDetails = myUserDetailsService.loadUserByUsername(username);
-            if(JwtUtils.isTokenExpired(Claims)){//token过期
+            if (JwtUtils.isTokenExpired(Claims)) {//token过期
                 System.out.println("token过期" + authToken);
-            }else{
+            } else {
                 System.out.println("token没过期，放行" + authToken);
                 //这里只要告诉springsecurity权限即可，账户密码就不用提供验证了，这里我们把UserDetails传给springsecurity，以便以后我们获取当前登录用户
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(UserDetails, null, UserDetails.getAuthorities());
